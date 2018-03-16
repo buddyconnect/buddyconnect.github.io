2c2a82c6e796ff20476884ae7bf3a39ffe15fb85
anshulprakash <anshul.prakash17@gmail.com>
2018-03-15 23:50:09 -0500

    Bootstrap incorporated

---
+++ js/index.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function () {
+    
+    jQuery('#showcase-typed').typed({
+        strings: ['Oh Hello!!', 'Help shall be provided for those who ask for it!', 'Because TAMU is as magical as Hogwarts!!','Scroll down to know more about what\'s in store for y\'all.'],
+        loop: true,
+        typeSpeed: 30,
+        backDelay: 2000,
+        loopCount: false
+    });
+    
+});
